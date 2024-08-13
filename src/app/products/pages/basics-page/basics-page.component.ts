f603843bd2be97a42ddae8aeb7fd19c4eea8743a
Rubén <rsacri27@hotmail.com>
2024-08-13 19:14:20 +0200

    create uppercase, lowercase, titlecase pipes

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string ='ruben';
+  public nameUpper: string ='RUBEN';
+  public fullName: string ='rUbEn';
+
 }
